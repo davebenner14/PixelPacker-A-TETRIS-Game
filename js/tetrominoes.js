729fb6979b0ba36928c4420bd3b404453a5ad0a6
David Benner <119465113+davebenner14@users.noreply.github.com>
2023-09-13 15:06:26 -0400

    Add tetrominoes rough

---
+++ js/tetrominoes.js
@@ -0,0 +1,48 @@
+const TETROMINOES = {
+  I: {
+    shape: [[1, 1, 1, 1]],
+    color: "cyan",
+  },
+  O: {
+    shape: [
+      [1, 1],
+      [1, 1],
+    ],
+    color: "yellow",
+  },
+  T: {
+    shape: [
+      [0, 1, 0],
+      [1, 1, 1],
+    ],
+    color: "purple",
+  },
+  S: {
+    shape: [
+      [0, 1, 1],
+      [1, 1, 0],
+    ],
+    color: "green",
+  },
+  Z: {
+    shape: [
+      [1, 1, 0],
+      [0, 1, 1],
+    ],
+    color: "red",
+  },
+  J: {
+    shape: [
+      [1, 0, 0],
+      [1, 1, 1],
+    ],
+    color: "blue",
+  },
+  L: {
+    shape: [
+      [0, 0, 1],
+      [1, 1, 1],
+    ],
+    color: "orange",
+  },
+};
